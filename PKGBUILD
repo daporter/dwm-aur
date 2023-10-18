9f8ac1e56675b0df6ae8ccbef1f7bbbacd0bd990
David Porter <david@daporter.net>
2023-10-19 00:07:13 +0200

    Add patches; tweak colors
    Patches added:
    - titlecolor
    - uselessgap

--- PKGBUILD
+++ PKGBUILD
@@ -16,17 +16,26 @@ install=dwm.install
 source=(dwm.desktop
         https://dl.suckless.org/dwm/dwm-$pkgver.tar.gz
         config.h
-        https://dwm.suckless.org/patches/shift-tools/shift-tools.c)
+        https://dwm.suckless.org/patches/shift-tools/shift-tools.c
+        https://dwm.suckless.org/patches/titlecolor/dwm-titlecolor-20210815-ed3ab6b4.diff
+        https://dwm.suckless.org/patches/uselessgap/dwm-uselessgap-20211119-58414bee958f2.diff)
 sha256sums=('bc36426772e1471d6dd8c8aed91f288e16949e3463a9933fee6390ee0ccd3f81'
             'fa9c0d69a584485076cfc18809fd705e5c2080dafb13d5e729a3646ca7703a6e'
-            'SKIP')
+            '735e0191e9edda363c32edecb3f4e85460427397cb0a828e18b024e852ac6b5c'
+            'ea03163c888c15fb3e7fe847ac8bb3fd13bbb4fe7982157b9227bdf11ae10306'
+            '08974ad6fb8f32938b7a793ecc6e281c7bbdfe8f40d1ed52c2ef31f62738f5c6'
+            '80cb7a75ae1f38fe7ca167d636fb8e90506dddd6165c2f32cbb0dd1b02eff576')
 
 prepare() {
   cd "$srcdir/$pkgname-$pkgver"
   if [[ -f "$srcdir/config.h" ]]; then
     cp -fv "$srcdir/config.h" config.h
   fi
-  cp "$srcdir/shift-tools.c" shift-tools.c
+  if [[ -f "$srcdir/shift-tools.c" ]]; then
+    cp -fv "$srcdir/shift-tools.c" shift-tools.c
+  fi
+  patch --forward --strip=1 --input="${srcdir}/dwm-titlecolor-20210815-ed3ab6b4.diff"
+  patch --forward --strip=1 --input="${srcdir}/dwm-uselessgap-20211119-58414bee958f2.diff"
 }
 
 build() {
@@ -41,7 +50,3 @@ package() {
   install -Dm644 README "$pkgdir/usr/share/doc/$pkgname/README"
   install -Dm644 "$srcdir/dwm.desktop" "$pkgdir/usr/share/xsessions/dwm.desktop"
 }
-sha256sums=('bc36426772e1471d6dd8c8aed91f288e16949e3463a9933fee6390ee0ccd3f81'
-            'fa9c0d69a584485076cfc18809fd705e5c2080dafb13d5e729a3646ca7703a6e'
-            'd135170b241b139f34d5559f584e9667dd09f191edbeb1fe6e3e908c8c8efc47'
-            'ea03163c888c15fb3e7fe847ac8bb3fd13bbb4fe7982157b9227bdf11ae10306')
--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -9,14 +10,18 @@ static const char *fonts[]          = { "Iosevka Nerd Font-9" };
 static const char dmenufont[]       = "monospace-8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#777777";
+static const char col_gray4[]       = "#bbbbbb";
+static const char col_gray5[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_plum[]        = "#52154e";
+static const char col_black[]       = "#000000";
+static const char col_red[]         = "#9b1e1c";
+static const char col_blue[]         = "#008080";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_plum  },
+	/*               fg          bg         border   */
+	[SchemeNorm]  = { col_gray4, col_black, col_gray2 },
+	[SchemeSel]   = { col_red,   col_black, col_gray4 },
+	[SchemeTitle] = { col_gray3, col_black, col_black },
 };
 
 /* tagging */
@@ -35,7 +40,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -134,4 +139,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
