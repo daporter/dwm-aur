23a4aa7c332bb9d0084fd01e4bf7f75dca9eb7ec
David Porter <david@daporter.net>
2023-10-19 23:22:34 +0200

    Improve colours

--- config.h
+++ config.h
@@ -6,22 +6,21 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Nerd Font-9" };
-static const char dmenufont[]       = "monospace-8";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#777777";
-static const char col_gray4[]       = "#bbbbbb";
-static const char col_gray5[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Iosevka Comfy Duo:size=9",
+                                        "Iosevka Nerd Font:size=9" };
+static const char dmenufont[]       = "Iosevka Comfy Duo:size=9";
+static const char col_gray1[]       = "#eeeeee";
+static const char col_gray2[]       = "#bbbbbb";
+static const char col_gray3[]       = "#989898";
+static const char col_gray4[]       = "#535353";
+static const char col_red1[]        = "#ff5f59";
+static const char col_red2[]        = "#702020";
 static const char col_black[]       = "#000000";
-static const char col_red[]         = "#9b1e1c";
-static const char col_blue[]         = "#008080";
 static const char *colors[][3]      = {
-	/*               fg          bg         border   */
-	[SchemeNorm]  = { col_gray4, col_black, col_gray2 },
-	[SchemeSel]   = { col_red,   col_black, col_gray4 },
-	[SchemeTitle] = { col_gray3, col_black, col_black },
+	/*                fg         bg         border   */
+	[SchemeNorm]  = { col_gray2, col_black, col_gray4 },
+	[SchemeSel]   = { col_gray1, col_red2,  col_gray3 },
+	[SchemeTitle] = { col_red1,  col_black, col_black },
 };
 
 /* tagging */
@@ -63,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray2, "-sb", col_red2, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 #include "shift-tools.c"
